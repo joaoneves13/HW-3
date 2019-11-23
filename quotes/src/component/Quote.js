35664630755a837491c3b27c8a911816997f27f7
joaoneves13 <13joaoneves@gmail.com>
2019-11-23 18:05:13 +0100

    Step 4

--- quotes/src/component/Quote.js
+++ quotes/src/component/Quote.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class Quote extends Component {
+
     render() {
         return (
         <div>
             <p>{this.props.quoteText}</p>
-            <p>By: {this.props.quoteAuthor}</p>
+            <p>By: {this.props.quoteAuthor}
+            <button onClick={this.liked}>:)</button>
+            <button onClick={this.dislike}>:(</button>
+            </p>
         </div>
             )}  
         }
